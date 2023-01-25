2dabd6f08ff2596a798142fc9464a780838199fe
Ben <vvvvvben@icloud.com>
2023-01-25 17:06:28 +0800

    客户端主函数

--- client/src/main/java/cn/niu/client/RpcClient.java
+++ client/src/main/java/cn/niu/client/RpcClient.java
@@ -1,5 +1,15 @@
 package cn.niu.client;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -10,6 +20,36 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RpcClient {
     public static void main(String[] args) {
+        log.info("client start..");
 
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
+        //MessageCodecSharable MESSAGE_CODEC = new MessageCodecSharable();
+
+        // rpc 响应消息处理器，待实现
+        //RpcResponseMessageHandler RPC_HANDLER = new RpcResponseMessageHandler();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.group(group);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    //ch.pipeline().addLast(new ProcotolFrameDecoder());
+                    ch.pipeline().addLast(LOGGING_HANDLER);
+                    //ch.pipeline().addLast(MESSAGE_CODEC);
+                    //ch.pipeline().addLast(RPC_HANDLER);
+                }
+            });
+            Channel channel = bootstrap.connect("localhost", 8080).sync().channel();
+
+            log.info("client started");
+            channel.closeFuture().sync();
+        } catch (Exception e) {
+            log.error("客户端发生异常，{}", e.toString());
+        } finally {
+            group.shutdownGracefully();
+            log.info("client closed..");
+        }
     }
 }
