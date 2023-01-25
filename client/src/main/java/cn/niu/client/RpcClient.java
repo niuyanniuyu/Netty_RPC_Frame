f4d14781c7a69aceb0d79c328448f7823d44cce7
Ben <vvvvvben@icloud.com>
2023-01-25 16:49:43 +0800

    RPC框架项目结构初始化

---
+++ client/src/main/java/cn/niu/client/RpcClient.java
@@ -0,0 +1,15 @@
+package cn.niu.client;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * RPC客户端
+ *
+ * @author Ben
+ */
+@Slf4j
+public class RpcClient {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ server/src/main/java/cn/niu/server/RpcServer.java
@@ -0,0 +1,15 @@
+package cn.niu.server;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * PRC服务端
+ *
+ * @author Ben
+ */
+@Slf4j
+public class RpcServer {
+    public static void main(String[] args) {
+
+    }
+}
